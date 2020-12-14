90998567be1208566a480a83e8d34dab7399670d
Vinicius Mazon <vnpmazon@gmail.com>
2020-12-13 23:38:25 -0300

    chore(mobile): Changes the project name

--- mobile/android/app/src/main/java/com/authrn/MainActivity.java
+++ mobile/android/app/src/main/java/com/authrn/MainActivity.java
@@ -1,4 +1,4 @@
-package com.authrn;
+package com.mobile;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "authrn";
+    return "mobile";
   }
 }
--- mobile/ios/authrnTests/authrnTests.m
+++ mobile/ios/authrnTests/authrnTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface authrnTests : XCTestCase
+@interface mobileTests : XCTestCase
 
 @end
 
-@implementation authrnTests
+@implementation mobileTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
